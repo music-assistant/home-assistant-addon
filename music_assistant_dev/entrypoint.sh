e5e7e1bed84a399a2dad52f6091b17d41018632b
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-09-04 13:58:40 +0200

    fix entrypoint.sh

--- music_assistant_dev/entrypoint.sh
+++ music_assistant_dev/entrypoint.sh
@@ -4,6 +4,6 @@ branch=$(cat /data/options.json | jq -r .branch)
 
 echo "Installing latest version from $branch..."
 
-uv pip install git+https://github.com/music-assistant/server.git@$branch
+uv pip install "music-assistant[server]@git+https://github.com/music-assistant/server.git@$branch"
 
 mass --config /data
