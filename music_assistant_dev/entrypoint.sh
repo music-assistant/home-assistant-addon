5a2128e7bcd843c34b1b538bcc923339bcecf69f
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-09-04 14:06:09 +0200

    Update entrypoint.sh

--- music_assistant_dev/entrypoint.sh
+++ music_assistant_dev/entrypoint.sh
@@ -4,6 +4,9 @@ branch=$(cat /data/options.json | jq -r .branch)
 
 echo "Installing latest version from $branch..."
 
-uv pip install "music-assistant[server]@git+https://github.com/music-assistant/server.git@$branch"
+uv pip install \
+    --no-cache \
+    --find-links "https://wheels.home-assistant.io/musllinux/" \
+    "music-assistant[server]@git+https://github.com/music-assistant/server.git@$branch"
 
 mass --config /data
