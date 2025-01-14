4cccd89bc4ee1c0ed5a30c23971f4a0eb627b7f6
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-01-14 13:20:37 +0100

    bump dev addon to 1.2.1

--- music_assistant_dev/entrypoint.sh
+++ music_assistant_dev/entrypoint.sh
@@ -6,7 +6,6 @@ echo "Installing latest version from $branch..."
 
 uv pip install \
     --no-cache \
-    --find-links "https://wheels.home-assistant.io/musllinux/" \
-    "music-assistant[server]@git+https://github.com/music-assistant/server.git@$branch"
+    "music-assistant@git+https://github.com/music-assistant/server.git@$branch"
 
 mass --config /data
