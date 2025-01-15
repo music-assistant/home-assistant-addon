027fb96d839a71815eb79631fbed2cfd70e8e251
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-01-15 21:44:18 +0100

    dev addon 1.2.3

--- music_assistant_dev/entrypoint.sh
+++ music_assistant_dev/entrypoint.sh
@@ -2,10 +2,20 @@
 
 branch=$(cat /data/options.json | jq -r .branch)
 
-echo "Installing latest version from $branch..."
-
+echo ""
+echo "-----------------------------------------------------------"
+echo "Music Assistant dev/nightly version"
+echo "-----------------------------------------------------------"
+echo ""
+echo "Installing latest version from $branch in $VIRTUAL_ENV"
+echo ""
+uv venv $VIRTUAL_ENV
 uv pip install \
     --no-cache \
+    --link-mode=copy \
     "music-assistant@git+https://github.com/music-assistant/server.git@$branch"
 
+echo ""
+echo ""
+source $VIRTUAL_ENV/bin/activate
 mass --config /data
