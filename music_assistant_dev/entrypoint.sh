8d1fccf69eb992be9c7c7ee97d2aa8e7082c01f7
Marvin Schenkel <marvinschenkel@gmail.com>
2025-09-05 11:15:15 +0200

    Update entrypoint.sh

--- music_assistant_dev/entrypoint.sh
+++ music_assistant_dev/entrypoint.sh
@@ -10,9 +10,13 @@ echo ""
 echo "Installing latest version from $branch in $VIRTUAL_ENV"
 echo ""
 source $VIRTUAL_ENV/bin/activate
+# Needed build dependency for librosa
+uv pip install scikit-build-core
+
 uv pip install \
     --no-cache \
     --link-mode=copy \
+    --no-build-isolation \
     "music-assistant@git+https://github.com/music-assistant/server.git@$branch"
 
 echo ""
